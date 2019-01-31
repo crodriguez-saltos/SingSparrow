7ac03c24490efa02a69865351752f0f879e9d26b
Carlos Rodriguez-Saltos <carodr5@emory.edu>
2019-01-31 09:41:07 -0500

    Change default end hour
    The hour when to close SingSparrow, according to install-operant-yoked.sh, has been updated.

--- install-operant-yoked.sh
+++ install-operant-yoked.sh
@@ -6,6 +6,6 @@ echo "This is the installation utility for SingSparrow Operant Yoked mode"
 
 # Schedule Rpi to run on a daily basis
 echo "00 08 * * * DISPLAY=:0 /home/pi/SingSparrow/singsparrow-operant-yoked.sh" > mycron
-echo "00 15 * * * /home/pi/SingSparrow/shutsparrow.sh /home/pi/SingSparrow/processes" >> mycron
+echo "00 19 * * * /home/pi/SingSparrow/shutsparrow.sh /home/pi/SingSparrow/processes" >> mycron
 crontab mycron
 rm mycron
