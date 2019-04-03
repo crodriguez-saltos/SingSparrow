2b5ea135f3254d3e41758c60ad33d2b0ce68add3
Carlos Rodriguez-Saltos <carodr5@emory.edu>
2019-04-03 15:19:36 -0400

    Add list of sounds to use to test identity
    These sounds can be uniquely paired with a booth or sound system to test whether they are connected to the right hardware rununing SingSparrow.

--- audio/README.txt
+++ audio/README.txt
@@ -1,1 +1,10 @@
 EBeq refers to an equalizer applied to compensate for a slight frequency distortion present in Logitech speakers (model??) used at the Maney Lab. EB stands for Eric Broner, who designed the filter.
+
+This folder includes wave files used for audio tests of booth identity (see audiotest.sh). The wave files were derived from the following recordings downloaded from Xeno-Canto:
+
+forsters-tern.wav: XC408570-Sterna_forsteri-FL perched Upper Newport Bay Nature Preserve, Orange Co 17May15 7.10am LS116414a.mp3
+acadian-flycatcher.wav: XC420873-Empidonax virescens_Michigan_ML0282080.mp3
+dark-eyed-junco.wav: XC449492-Dark-eyed Junco m, oregon, at 2nd -CA, Fort Rosecrans NC, October 21, 2018, 0910 AM.mp3
+eastern-bluebird.wav: XC452139-BLUEBIR_Eastern fulva 2nd Mesa del C Yecora 2000m 071006 0645.mp3
+carolina-wren.wav: XC452779-Carolina Wren 2 -TX, Estero Llano Grande SP, December 17, 2017, 0701 AM.mp3
+blue-jay.wav: XC463860-20190328-BlueJayCall.mp3
--- audiotest.sh
+++ audiotest.sh
@@ -2,12 +2,11 @@
 # This program contains useful functions to test audio for SingSparrow.
 
 # Import variables
-while getopts "t:ap" option
+while getopts "t:p:" option
 do
     case "${option}"
     in
 	t) test=${OPTARG};;
-	a) assignation=${OPTARG};;
 	p) params=${OPTARG};;
     esac
 done
@@ -35,13 +34,18 @@ fi
 # With this test, a sound is played to check that the playback system is hooked to the right booth or sound system. For this test to work, a unique sound must be uniquely assigned to any particular system
 
 if [ "$test" == "id" ]; then
-    # Load assignations
-    booth=$(cat $params | grep "booth = " | sed "s/booth = \(.*\)/\1/")
-    assigned=$(cat $assignation | grep "$booth" | cut -f 2)
-    
     echo "Identity test was selected"
     echo "A sound will be played continuously for 5 minutes, or until the user forcefully terminates the program."
     echo "To break the loop, type Ctrl+C"
+    
+    # Load assignations
+    assignation="./audio/id-test-assignments.txt"
+    echo "Assignation table is $assignation"
+    booth=$(cat $params | grep "booth = " | sed "s/\r//" | sed "s/booth = \(.*\)/\1/")
+    assigned=$(cat $assignation | grep "$booth" | cut -d " " -f 2)
+   
+    echo "Booth is $booth"
+    echo "Sound is $assigned"
 
     now=$(date +%s)
     while :; do
@@ -49,7 +53,7 @@ if [ "$test" == "id" ]; then
 	if [ "$elapsed" == "5" ]; then
 	    break
 	fi
-	aplay $assigned
-	sleep 2
+	aplay ./audio/$assigned
+	sleep 1.5
     done
 fi
